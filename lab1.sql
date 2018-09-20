b4475b7f0f0adcfefa1573cf26a328fb320f72d7
Md Nabil Ahsan <nabilahsan@users.noreply.github.com>
2018-09-20 12:19:30 -0500

    StudentCourse table cannot be added.
    Everything else works.

--- lab1.sql
+++ lab1.sql
@@ -2,6 +2,12 @@
 -- Added NOT NULL to PKs.
 -- Checking for same data constraints on all attributes.
 
+
+-- Add PK on table the FK is referencing to
+-- 
+-- For Example:
+-- Change this: FOREIGN KEY (TutorKey) REFERENCES Tutor
+-- To this: FOREIGN KEY (TutorKey) REFERENCES Tutor (TutorKey),
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
@@ -32,17 +38,16 @@ EthnicityDescription CHAR(45),
 PRIMARY KEY (EthnicityKey)
 );
 
--- Cannot Add
--- ERROR 1215 (HY000): Cannot add foreign key constraint
+
 CREATE TABLE TutorCourse (
 TutorKey INT NOT NULL ,
 CourseKey CHAR(6) NOT NULL ,
 PRIMARY KEY (TutorKey, CourseKey),
-FOREIGN KEY (CourseKey) REFERENCES Course,
-FOREIGN KEY (TutorKey) REFERENCES Tutor
+FOREIGN KEY (TutorKey) REFERENCES Tutor (TutorKey),
+FOREIGN KEY (CourseKey) REFERENCES Course (CourseKey)
 );
 
--- ERROR 1215 (HY000): Cannot add foreign key constraint
+
 create table Students( 
 StudentKey INT NOT NULL, 
 StudentFirstName CHAR(20), 
@@ -55,16 +60,21 @@ StudentCitizen CHAR(5),
 StudentWorkerRetraining CHAR(20), 
 EthnicityKey CHAR(20) NOT NULL,
 PRIMARY KEY (EthnicityKey, StudentKey), 
-FOREIGN KEY (EthnicityKey) REFERENCES Ethnicity);
+FOREIGN KEY (EthnicityKey) REFERENCES Ethnicity(EthnicityKey)
+);
 
-
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- Cannot Add
+-- ERROR 1215 (HY000): Cannot add foreign key constraint
 CREATE TABLE StudentCourse (
 StudentKey INT NOT NULL ,
 CourseKey CHAR(6) NOT NULL,
 StudentCourseQuarter CHAR(45),
 PRIMARY KEY (StudentKey, CourseKey),
-FOREIGN KEY (StudentKey) REFERENCES Students
+FOREIGN KEY (StudentKey) REFERENCES Students(StudentKey),
+FOREIGN KEY (CourseKey) REFERENCES Course(CourseKey)
 );
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
 
 -- Added
 CREATE TABLE Sessions (
@@ -90,48 +100,6 @@ PRIMARY KEY (RequestKey)
 -- Added
 CREATE TABLE RequestNote (
 RequestNoteKey DATETIME NOT NULL,
-RequestID INT,
-RequestNoteText CHAR(100),
-PRIMARY KEY (RequestNoteKey)
-);
-
-,
-PRIMARY KEY (EthnicityKey, StudentKey), 
-FOREIGN KEY (EthnicityKey) REFERENCES Ethnicity);
-
-
-CREATE TABLE StudentCourse (
-StudentKey INT ,
-CourseKey CHAR(6) ,
-StudentCourseQuarter CHAR(45),
-PRIMARY KEY (StudentKey,CourseKey),
-FOREIGN KEY (StudentKey) REFERENCES Students
-);
-
--- Added
-CREATE TABLE Sessions (
-SessionDateKey DATE ,
-SessionTimeKey DATETIME,
-TutorKey INT,
-CourseKey CHAR(45),
-SessionStatus CHAR(45),
-SessionMaterialCovered CHAR(45),
-PRIMARY KEY (SessionDateKey)
-);
-
--- Added
-CREATE TABLE Requests (
-RequestKey INT ,
-RequestDate DATE,
-CourseKey CHAR(6) ,
-RequestStatus CHAR(45),
-StudentKey INT ,
-PRIMARY KEY (RequestKey)
-);
-
--- Added
-CREATE TABLE RequestNote (
-RequestNoteKey DATETIME ,
 RequestID INT,
 RequestNoteText CHAR(100),
 PRIMARY KEY (RequestNoteKey)
