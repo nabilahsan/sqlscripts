df5f4a66ffc87affeb0de66b9031d7f1fece8572
Md Nabil Ahsan <nabilahsan@users.noreply.github.com>
2018-09-20 00:09:48 -0500

    Shows whats added.
    Issues with foreign key

--- lab1.sql
+++ lab1.sql
@@ -1,3 +1,4 @@
+-- Added
 CREATE TABLE Tutor (
 TutorKey INT ,
 TutorLastName CHAR(45),
@@ -9,6 +10,7 @@ TutorStatus CHAR(45),
 PRIMARY KEY (TutorKey)
 );
 
+-- Added
 CREATE TABLE Course (
 CourseKey CHAR(6) ,
 CourseName CHAR(45),
@@ -16,23 +18,26 @@ CourseDescription CHAR(45),
 PRIMARY KEY (CourseKey)
 );
 
+-- Added
 CREATE TABLE Ethnicity (
 EthnicityKey CHAR(45) ,
 EthnicityDescription CHAR(45),
 PRIMARY KEY (EthnicityKey)
 );
 
+-- Cannot Add
+-- ERROR 1215 (HY000): Cannot add foreign key constraint
 CREATE TABLE TutorCourse (
 TutorKey INT ,
 CourseKey CHAR(45) ,
-KEY TutorKey (TutorKey),
-KEY CourseKey (CourseKey),
-CONSTRAINT CourseKey FOREIGN KEY (CourseKey) REFERENCES Course (CourseKey),
-CONSTRAINT TutorKey FOREIGN KEY (TutorKey) REFERENCES Tutor (TutorKey) 
+PRIMARY KEY (TutorKey),
+FOREIGN KEY (CourseKey) REFERENCES Course,
+FOREIGN KEY (TutorKey) REFERENCES Tutor
 );
 
+-- ERROR 1215 (HY000): Cannot add foreign key constraint
 create table Students( 
-StudentKey INT PRIMARY KEY, 
+StudentKey INT, 
 StudentFirstName CHAR(20), 
 StudentLastName CHAR(20), 
 StudentEmail CHAR(20), 
@@ -41,7 +46,8 @@ StudentGender CHAR(5),
 StudentAge INT, 
 StudentCitizen CHAR(5), 
 StudentWorkerRetraining CHAR(20), 
-EthnicityKey CHAR(20), 
+EthnicityKey CHAR(20),
+PRIMARY KEY (EthnicityKey, StudentKey), 
 FOREIGN KEY (EthnicityKey) REFERENCES Ethnicity);
 
 
@@ -50,32 +56,33 @@ StudentKey INT ,
 CourseKey CHAR(6) ,
 StudentCourseQuarter CHAR(45),
 PRIMARY KEY (StudentKey,CourseKey),
-KEY CourseKey (CourseKey,StudentKey),
-CONSTRAINT StudentKey FOREIGN KEY (StudentKey) REFERENCES Student (StudentKey)
+FOREIGN KEY (StudentKey) REFERENCES Students
 );
 
+-- Added
 CREATE TABLE Sessions (
 SessionDateKey DATE ,
-SessionTimeKey DATEtime,
+SessionTimeKey DATETIME,
 TutorKey INT,
 CourseKey CHAR(45),
 SessionStatus CHAR(45),
 SessionMaterialCovered CHAR(45),
-PRIMARY KEY (SessionDATEKey)
+PRIMARY KEY (SessionDateKey)
 );
 
+-- Added
 CREATE TABLE Requests (
 RequestKey INT ,
 RequestDate DATE,
 CourseKey CHAR(6) ,
 RequestStatus CHAR(45),
 StudentKey INT ,
-PRIMARY KEY (RequestKey),
-KEY CourseKey_idx (CourseKey)
+PRIMARY KEY (RequestKey)
 );
 
+-- Added
 CREATE TABLE RequestNote (
-RequestNoteKey DATEtime ,
+RequestNoteKey DATETIME ,
 RequestID INT,
 RequestNoteText CHAR(100),
 PRIMARY KEY (RequestNoteKey)
