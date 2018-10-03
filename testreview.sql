706682f7f18617a08e39515522bb3c78284c66d9
nabilahsan <nabilahsan95@gmail.com>
2018-10-03 00:09:05 -0500

    added tables and data
    Signed-off-by: nabilahsan <nabilahsan95@gmail.com>

--- testreview.sql
+++ testreview.sql
@@ -25,7 +25,13 @@ department char(15),
 faculRank char(25),
 PRIMARY KEY (facId)
 );
-insert into values();
+--
+
+insert into Faculty values('F101','Adams', 'Art', 'Professor');
+insert into Faculty values('F105','Tanaka', 'CSC', 'Instructor');
+insert into Faculty values('F110','Byrne', 'Math', 'Assistant');
+insert into Faculty values('F115','Smith', 'History', 'Associate');
+insert into Faculty values('F221','Smith', 'CSC', 'Professor');
 --
 
 create table Classes(
@@ -36,10 +42,15 @@ room char(10),
 PRIMARY KEY(classNumber, facId),
 FOREIGN KEY (facId) REFERENCES Faculty(facId)
 );
-insert into values();
+
+insert into Classes values('ART103A', 'F101','MWF9', 'H221');
+insert into Classes values('CSC201A', 'F105','TUTHF10', 'M110');
+insert into Classes values('CSC203A', 'F105','MTHF12', 'M110');
+insert into Classes values('HST205A', 'F115','MWF11', 'H221');
+insert into Classes values('MTH101B', 'F110','MTUTH9', 'H225');
+insert into Classes values('MTH103C', 'F110','MWF11', 'H225');
 --
 
-drop table Classes;
 
 create table Enroll(
 stuId CHAR(5) not null,
@@ -49,6 +60,14 @@ PRIMARY KEY(stuId, classNumber),
 FOREIGN KEY (stuId) REFERENCES Student(stuId) 
 );
 
-insert into values();
+insert into Enroll values('S1001', 'ART103A','A');
+insert into Enroll values('S1001', 'HST205A','C');
+insert into Enroll values('S1002', 'ART103A','D');
+insert into Enroll values('S1002', 'CSC201A','F');
+insert into Enroll values('S1002', 'MTH103C','B');
+insert into Enroll values('S1010', 'ART103A', null);
+insert into Enroll values('S1010', 'MTH103C', null);
+insert into Enroll values('S1020', 'CSC201A','B');
+insert into Enroll values('S1020', 'MTH101B','A');
 --
 
